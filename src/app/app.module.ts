83d2219faf3053b4f65fd8d814aef0657705ce71
Kleber Vasconcelos <klebervasconcelos@MacBook-Pro-de-Kleber.local>
2022-10-21 15:19:15 -0300

    Card component added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CardComponent } from './components/card/card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/task.model.ts
@@ -0,0 +1,7 @@
+export interface Task {
+  title: string;
+  description: string;
+  place: string;
+  duration_time: number;
+  date_hour: Date;
+}
